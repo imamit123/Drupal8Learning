ab460ac491af0eb402d1dc4008464e72847fca5c
Amit Kumar <amit.k@gai.co.in>
2017-06-02 12:46:44 +0530

    change in files

--- psi/js/scroll_progress_line.js
+++ psi/js/scroll_progress_line.js
@@ -1,17 +1,20 @@
-(function ($) {
-  'use strict';
-  Drupal.behaviors.scrolljs = {
-    attach: function (context, settings) {
-      var color = Drupal.settings.scroll_progress_color;
+ (function($) {
+      Drupal.behaviors.psijs = {
+        attach: function (context, settings) {//alert('asas');
+            // var color = Drupal.settings.scroll_progress_color;
+             var $color = drupalSettings.psi.colour;
       $('body').append('<div class="bar-long"></div>');
       var scrollPercent = 100 * $(window).scrollTop() / ($(document).height() - $(window).height());
       $('.bar-long').css('width', scrollPercent + '%');
-      $('.bar-long').css('background-color', color);
-      $('.bar-long').css('box-shadow', '0 0 8px ' + color);
+      $('.bar-long').css('background-color', $color);
+      $('.bar-long').css('box-shadow', '0 0 8px ' + $color);
       $(window).scroll(function () {
         var scrollPercent = 100 * $(window).scrollTop() / ($(document).height() - $(window).height());
         $('.bar-long').css('width', scrollPercent + '%');
       });
-    }
-  };
-}(jQuery));
+       alert('123');
+        }
+
+      };
+
+})(jQuery);
--- psi/src/Form/PsiConfigureForm.php
+++ psi/src/Form/PsiConfigureForm.php
@@ -35,62 +35,92 @@ class PsiConfigureForm extends ConfigFormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $psi_config = $this->config('psi.configure');
-    $form['msg_text'] = array(
-      '#title' => t('Text'),
-      '#type' => 'textarea',
-      '#description' => t('Insert text which will appear on the bar. You may also include HTML.'),
-      '#default_value' => $psi_config->get('msg_text'),
-    );
-    $form['msg_color'] = array(
-      '#title' => t('Color'),
-      '#type' => 'textfield',
-      '#description' => t('Insert the color code : example - "#FFFF00" or "red".'),
-      '#default_value' => $psi_config->get('msg_color'),
-    );
-    $form['msg_color_hover'] = array(
-      '#title' => t('Hover color'),
-      '#type' => 'textfield',
-      '#description' => t('Insert the color code for mouse hover : example - "#FFFF00" or "red".'),
-      '#default_value' => $psi_config->get('msg_color_hover'),
-    );
-    $form['msg_delaytime'] = array(
-      '#title' => t('Delay Time'),
-      '#type' => 'textfield',
-      '#description' => t('Insert the delay time: example - "5000" = 5 sec.'),
-      '#default_value' => $psi_config->get('msg_delaytime'),
-    );
+
+ $form['scroll_progress_theme'] = array(
+    '#title' => t('Select theme for Scroll to use'),
+    '#description' => t('Scroll comes with a lot of themes for progress. Please select the one that you prefer.'),
+    '#type' => 'radios',
+    '#options' => array(
+      '1' => t('Straight line'),
+      '2' => t('Circular progress'),
+      '3' => t('Animated progress'),
+      '4' => t('Tooltip progress'),
+      '5' => t('Bottom line'),
+    ),
+    '#default_value' => $psi_config->get('scroll_progress_theme'),
+  );
+  $form['scroll_progress_color'] = array(
+    '#title' => t('Color code.'),
+    '#description' => t('Default color for scroll progress is #ff0000.'),
+    '#type' => 'textfield',
+    '#default_value' => $psi_config->get('scroll_progress_color'),
+  );
+  $form['scroll_progress_load_on_admin_enabled'] = array(
+    '#title' => t('Load in administration pages.'),
+    '#description' => t('SCROLL is disabled by default on administration pages. Check to enable'),
+    '#type' => 'checkbox',
+    '#default_value' => $psi_config->get('scroll_progress_load_on_admin_enabled'),
+  );
+
+
+
+
+    // $form['msg_text'] = array(
+    //   '#title' => t('Text'),
+    //   '#type' => 'textarea',
+    //   '#description' => t('Insert text which will appear on the bar. You may also include HTML.'),
+    //   '#default_value' => $psi_config->get('msg_text'),
+    // );
+    // $form['msg_color'] = array(
+    //   '#title' => t('Color'),
+    //   '#type' => 'textfield',
+    //   '#description' => t('Insert the color code : example - "#FFFF00" or "red".'),
+    //   '#default_value' => $psi_config->get('msg_color'),
+    // );
+    // $form['msg_color_hover'] = array(
+    //   '#title' => t('Hover color'),
+    //   '#type' => 'textfield',
+    //   '#description' => t('Insert the color code for mouse hover : example - "#FFFF00" or "red".'),
+    //   '#default_value' => $psi_config->get('msg_color_hover'),
+    // );
+    // $form['msg_delaytime'] = array(
+    //   '#title' => t('Delay Time'),
+    //   '#type' => 'textfield',
+    //   '#description' => t('Insert the delay time: example - "5000" = 5 sec.'),
+    //   '#default_value' => $psi_config->get('msg_delaytime'),
+    // );
     return parent::buildForm($form, $form_state);
   }
 
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    // Set default values if fields are empty.
-    if ($form_state->isValueEmpty('msg_text')) {
-      $form_state->setValueForElement($form['msg_text'], $this->t('This text will appear on Psi bar. You may also include HTML.'));
-    }
-    if ($form_state->isValueEmpty('msg_color')) {
-      $form_state->setValueForElement($form['msg_color'], $this->t('#EB593C'));
-    }
-    if ($form_state->isValueEmpty('msg_color_hover')) {
-      $form_state->setValueForElement($form['msg_color_hover'], $this->t('#EB593C'));
-    }
-    if ($form_state->isValueEmpty('msg_delaytime')) {
-      $form_state->setValueForElement($form['msg_delaytime'], $this->t('5000'));
-    }
-    parent::validateForm($form, $form_state);
-  }
+  // public function validateForm(array &$form, FormStateInterface $form_state) {
+  //   // Set default values if fields are empty.
+  //   if ($form_state->isValueEmpty('scroll_progress_theme')) {
+  //     $form_state->setValueForElement($form['scroll_progress_theme'], $this->t('This text will appear on Psi bar. You may also include HTML.'));
+  //   }
+  //   if ($form_state->isValueEmpty('msg_color')) {
+  //     $form_state->setValueForElement($form['msg_color'], $this->t('#EB593C'));
+  //   }
+  //   if ($form_state->isValueEmpty('msg_color_hover')) {
+  //     $form_state->setValueForElement($form['msg_color_hover'], $this->t('#EB593C'));
+  //   }
+  //   if ($form_state->isValueEmpty('msg_delaytime')) {
+  //     $form_state->setValueForElement($form['msg_delaytime'], $this->t('5000'));
+  //   }
+  //   parent::validateForm($form, $form_state);
+  // }
 
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('psi.configure')
-      ->set('msg_text', $form_state->getValue('msg_text'))
-      ->set('msg_color', $form_state->getValue('msg_color'))
-      ->set('msg_color_hover', $form_state->getValue('msg_color_hover'))
-      ->set('msg_delaytime', $form_state->getValue('msg_delaytime'))
+      ->set('scroll_progress_theme', $form_state->getValue('scroll_progress_theme'))
+      ->set('scroll_progress_color', $form_state->getValue('scroll_progress_color'))
+      ->set('scroll_progress_load_on_admin_enabled', $form_state->getValue('scroll_progress_load_on_admin_enabled'))
+     // ->set('msg_delaytime', $form_state->getValue('msg_delaytime'))
       ->save();
     parent::submitForm($form, $form_state);
   }
